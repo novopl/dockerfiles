ccf90ec29fa238338580b1f251c7bdb88b7abfcd
novo <novopl@gmail.com>
2018-09-04 11:55:32 +0100

    Cleanup

---
+++ python36/files/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="fakepkg",
+    version='0.0.0',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
